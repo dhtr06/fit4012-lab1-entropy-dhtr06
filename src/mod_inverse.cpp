98214cfb84cbea9e34d09ab14d74c2296f37108a
dhtr06 <trungxyz15@gmail.com>
2026-05-07 22:27:00 +0700

    Update mod_inverse.cpp

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -8,6 +8,7 @@ int gcd(int a, int b) {
         b = a % b;
         a = temp;
     }
+
     return a;
 }
 
@@ -19,22 +20,30 @@ int extended_euclid(int a, int b, int &x, int &y) {
     }
 
     int x1 = 0, y1 = 0;
+
     int g = extended_euclid(b, a % b, x1, y1);
+
     x = y1;
     y = x1 - (a / b) * y1;
+
     return g;
 }
 
 int mod_inverse(int a, int m) {
-    // TODO(student): implement modular inverse using extended_euclid()
-    // If inverse does not exist, return -1.
-    (void)a;
-    (void)m;
-    return -1;
+    int x = 0, y = 0;
+
+    int g = extended_euclid(a, m, x, y);
+
+    if (g != 1) {
+        return -1;
+    }
+
+    return (x % m + m) % m;
 }
 
 int main() {
     int a = 0, m = 0;
+
     cout << "Nhap a, m: ";
     cin >> a >> m;
 
@@ -44,8 +53,16 @@ int main() {
     }
 
     int inv = mod_inverse(a, m);
-    cout << "Nghich dao cua " << a << " mod " << m << " la: " << inv << '\n';
-    cout << "Kiem tra: " << a << " * " << inv << " % " << m
-         << " = " << (1LL * a * inv % m) << '\n';
+
+    cout << "Nghich dao cua "
+         << a << " mod " << m
+         << " la: " << inv << '\n';
+
+    cout << "Kiem tra: "
+         << a << " * " << inv
+         << " % " << m
+         << " = " << (1LL * a * inv % m)
+         << '\n';
+
     return 0;
 }
