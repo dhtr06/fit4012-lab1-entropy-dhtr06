0d6b8c290f51e4e870e572ff3f6c8e068bbe6122
dhtr06 <trungxyz15@gmail.com>
2026-05-08 08:32:49 +0700

    Update entropy_redundancy.cpp

--- src/entropy_redundancy.cpp
+++ src/entropy_redundancy.cpp
@@ -24,8 +24,6 @@ double calculate_entropy(const string &text) {
 }
 
 double calculate_redundancy(const string &text, int alphabet_size = 256) {
-    // TODO(student): implement redundancy = log2(N) - H(X)
-    // Hint: use calculate_entropy(text)
     (void)text;
     (void)alphabet_size;
     return -1.0;
